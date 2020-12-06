5e4e4022a8b309a4d0b15890bc43f95a00051ca0
ChakradharG <47364794+ChakradharG@users.noreply.github.com>
2020-12-06 18:32:02 +0530

    Updated Editor.py

--- Editor.py
+++ Editor.py
@@ -2,13 +2,13 @@ from moviepy.editor import VideoFileClip, concatenate_videoclips
 import os
 
 
-def combine(vidName, extension, timeStamps):
+def combine(vidName, extension, timeStamps, path):
 	'''Clipping small segments from the video and stitching them into a single video'''
 
-	delta = 15	#Seconds before and after the event
+	delta = 15	#%%Seconds before and after the event
 	print('\nGenerating Highlights')
-	os.chdir('..')
-	video = VideoFileClip(f'{os.getcwd()}/{vidName}.{extension}')
+	video = VideoFileClip(f'{path}.{extension}')
 	videoClips = list(map(lambda x: video.subclip(x-delta, x+delta), timeStamps))
 	finalClip = concatenate_videoclips(videoClips)
-	finalClip.write_videofile(f'{os.getcwd()}/{vidName}/Highlights.{extension}')
+	finalClip.write_videofile(f'{path}/Highlights.mp4', codec='libx264')	#%%Small size, inferior quality
+	# finalClip.write_videofile(f'{path}/Highlights.avi', codec='rawvideo')	#%%Large size, superior quality
